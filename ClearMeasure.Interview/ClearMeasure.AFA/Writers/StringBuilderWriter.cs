fa02d502b4c5eaedae6676ef44a8d8c21b408f9a
Justin Self <justn.self@gmail.com>
2014-11-27 22:09:14 -0600

    added strinbuilderWriter

---
+++ ClearMeasure.Interview/ClearMeasure.AFA/Writers/StringBuilderWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using ClearMeasure.AFA.Writers.Interfaces;
+
+namespace ClearMeasure.AFA.Writers
+{
+    public class StringBuilderWriter : IWriter
+    {
+        private readonly StringBuilder stringBuilder;
+
+        public StringBuilderWriter(StringBuilder stringBuilder)
+        {
+            this.stringBuilder = stringBuilder;
+        }
+
+        public void WriteLine()
+        {
+            stringBuilder.AppendLine();
+        }
+
+        public void WriteLine(string message)
+        {
+            stringBuilder.AppendLine(message);
+        }
+
+        public void Write(string message)
+        {
+            stringBuilder.Append(message);
+        }
+    }
+}
