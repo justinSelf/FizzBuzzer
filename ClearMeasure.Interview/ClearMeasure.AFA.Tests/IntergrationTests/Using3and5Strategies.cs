f97ba181578ba53978f9331c9cce599368a33eea
Justin Self <justn.self@gmail.com>
2014-11-27 22:07:25 -0600

    shelled out Integration test

--- ClearMeasure.Interview/ClearMeasure.AFA.Tests/IntergrationTests/Using3and5Strategies.cs
+++ ClearMeasure.Interview/ClearMeasure.AFA.Tests/IntergrationTests/Using3and5Strategies.cs
@@ -1,9 +1,39 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using System.Collections.Generic;
+using ClearMeasure.AFA.DivisorStrategies.Interfaces;
+using ClearMeasure.AFA.Writers.Interfaces;
+using NUnit.Framework;
 
 namespace ClearMeasure.AFA.Tests.IntergrationTests
 {
     [TestFixture]
     public class Using3And5Strategies
     {
+        private IEnumerable<IDivisorStrategy> strategies;
+        private long upperBound = 10;
+        private IWriter writer;
+
+        [Test]
+        public void RunTest()
+        {
+            GivenIHaveA3and5Stragies();
+            WhenIExecuteWithAnUpperBoundOf10();
+            ThenMyWriterShouldWriteTheAppropriateText();
+        }
+
+        private void GivenIHaveA3and5Stragies()
+        {
+
+        }
+
+        private void WhenIExecuteWithAnUpperBoundOf10()
+        {
+
+        }
+
+        private void ThenMyWriterShouldWriteTheAppropriateText()
+        {
+
+        }
     }
 }
