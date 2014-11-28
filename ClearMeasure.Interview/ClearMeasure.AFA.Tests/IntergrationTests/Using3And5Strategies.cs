3e49a2c8ea9d7169def99cf75ea63a3a7587ae61
Justin Self <justn.self@gmail.com>
2014-11-27 22:42:34 -0600

    second integration test complete

--- ClearMeasure.Interview/ClearMeasure.AFA.Tests/IntergrationTests/Using3And5Strategies.cs
+++ ClearMeasure.Interview/ClearMeasure.AFA.Tests/IntergrationTests/Using3And5Strategies.cs
@@ -14,39 +14,69 @@ namespace ClearMeasure.AFA.Tests.IntergrationTests
     public class Using3And5Strategies
     {
         private IEnumerable<IDivisorStrategy> strategies;
-        private long upperBound = 10;
+        private long upperBound;
         private IWriter writer;
         private FizzBuzzer fizzBuzzer;
         private StringBuilder stringBuilder;
+        private string expectedMessage;
 
         [Test]
-        public void RunTest()
+        public void RunTestWithUpperBoundof10()
         {
-            GivenIHaveA3and5andDefaultStragies();
+            GivenIHaveA3and5Stragies();
             WhenIExecuteWithAnUpperBoundOf10();
             ThenMyWriterShouldWriteTheAppropriateText();
         }
 
-        private void GivenIHaveA3and5andDefaultStragies()
+        [Test]
+        public void RunTestWithUpperBoundOf16()
+        {
+            GivenIHaveA3and5Stragies();
+            WhenIExecuteWithAnUpperBoundOf16();
+            ThenMyWriterShouldWriteTheAppropriateText();
+        }
+
+        private void WhenIExecuteWithAnUpperBoundOf16()
+        {
+            upperBound = 16;
+            stringBuilder = new StringBuilder();
+            fizzBuzzer = new FizzBuzzer(new StringBuilderWriter(stringBuilder), strategies);
+            fizzBuzzer.Execute(upperBound);
+            expectedMessage = @"1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+";
+        }
+
+        private void GivenIHaveA3and5Stragies()
         {
             strategies = new List<IDivisorStrategy>
             {
-                new DivisibleBy5(),
-                new DivisibleBy3()
+                new DivisibleBy3(),
+                new DivisibleBy5()
             };
         }
 
         private void WhenIExecuteWithAnUpperBoundOf10()
         {
+            upperBound = 10;
             stringBuilder = new StringBuilder();
             fizzBuzzer = new FizzBuzzer(new StringBuilderWriter(stringBuilder), strategies);
             fizzBuzzer.Execute(upperBound);
-
-        }
-
-        private void ThenMyWriterShouldWriteTheAppropriateText()
-        {
-            var expectedMessage = @"1
+            expectedMessage = @"1
 2
 Fizz
 4
@@ -57,6 +87,10 @@ Fizz
 Fizz
 Buzz
 ";
+        }
+
+        private void ThenMyWriterShouldWriteTheAppropriateText()
+        {
             stringBuilder.ToString().Should().Be(expectedMessage);
         }
     }
